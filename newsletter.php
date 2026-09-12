b1029d994a68a289cf624a9438eeb88be044ed56
Murtaza Ahmad <murtazaahmad2004@gmail.com>
2026-09-12 11:47:38 +0500

    commi this

--- newsletter.php
+++ newsletter.php
@@ -68,8 +68,8 @@ function sendMail(array $recipients, string $subject, string $body): bool {
         $mail->Host = "smtp.gmail.com";
         $mail->SMTPAuth = true;
 
-        $mail->Username = getenv('MAIL_USERNAME');
-        $mail->Password = getenv('MAIL_PASSWORD');
+        $mail->Username = 'masofthub@gmail.com';
+        $mail->Password = 'rwrb xcaa npcr oczp';
 
         $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
         $mail->Port = 465;
